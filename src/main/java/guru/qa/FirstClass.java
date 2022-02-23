6e391691d7b76d9880e3def48f5fc93f688a692a
amelin <g-amelin@mail.ru>
2022-02-23 23:50:53 +0300

    added some lesson examples

---
+++ src/main/java/guru/qa/FirstClass.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+public class FirstClass {
+
+    byte aByte; // 8 bit -128..127
+    short aShort; // 16 bit -32,768..32,767
+    int anInt; // 32 bit -2,147,483,648..2,147,483,647 or (-2)^31 .. (2^31) -1
+    long aLong; // 64 bit -9,223,372,036,854,775,808..9,223,372,036,854,775,807 or (-2)^63 .. 2^63-1
+
+    char aChar; // 0..65,535
+
+    float aFloat; //  32 bit -3.4E+38..3.4E+38 (стандарт IEEE 754)
+    double aDouble; // 64 bit -1.7E+308..1.7E+308 (стандарт IEEE 754)
+
+    boolean aBoolean; // true или false
+}
