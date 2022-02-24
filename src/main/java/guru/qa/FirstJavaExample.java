fa1ee1df15518d66f7a6fced1352971380bb505c
amelin <g-amelin@mail.ru>
2022-02-24 23:32:05 +0300

    added Maths operators

--- src/main/java/guru/qa/FirstJavaExample.java
+++ src/main/java/guru/qa/FirstJavaExample.java
@@ -9,21 +9,29 @@ public class FirstJavaExample {
         int a = 100;
         int b = 200;
         char aChar = 1;
-        float aFloat = 0.1f;
-        double aDouble = 0.1;
+        float aFloat = 0.123f;
+        double aDouble = 0.123;
 
         int c = a + b;
         int d = a - b;
+        double e = a * aDouble;
+        double f = a * aFloat;
 
 
-        System.out.println("\n" + "Тестовые данные: ");
+        System.out.println("\n" + "Тестовые данные: " + "\n");
         System.out.println("a = " + a + "; b = " + b + "; aByte = " + aByte + "; aShort = " + aShort + ";");
         System.out.println("aChar = " + aChar + "; aFloat = " + aFloat + "; aDouble = " + aDouble + ".");
-        System.out.println("\n" + "Результаты расчетов: ");
+        System.out.println("\n" + "Результаты расчетов: " + "\n");
 
         System.out.println("c = (a + b) = " + c);
         System.out.println("d = (a - b) = " + d);
-        System.out.println("(c * d) / 5 = " + ((c * d) / 5));
+        System.out.println("(c * d) / 5450 = " + ((c * d) / 5450));
+        System.out.println("e = (a * aDouble) = " + e);
+        System.out.println("f = (a * aFloat) = " + f);
+        System.out.println("(c * e) / 5450 = " + ((c * e) / 5450 + "\n"));
+        System.out.println("(c * f) = " + c * f);
+        System.out.println("(c * f) / 5450 = " + ((c * f) / 5450));
+        System.out.println("(c * f) % 5 = " + ((c * f) % 3690));
         System.out.println(a++); // будет выведен результат до подсчета инкремента, а потом посчитан инкремент и сохранен
         System.out.println(++a);
     }
